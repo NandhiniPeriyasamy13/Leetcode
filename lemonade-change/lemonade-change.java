edb884da203d863cea978a0dc25e32e386751a6f
Nandhini Periyasamy <nandhiniperiyasamy13@gmail.com>
2021-05-14 12:28:51 -0500

    Time: 1 ms (100.00%), Space: 39.8 MB (59.11%) - LeetHub

---
+++ lemonade-change/lemonade-change.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        int twenty = 0;
+        for(int i=0;i<bills.length;i++){
+            if(bills[i] == 5){
+                five++;
+            }else if(bills[i] == 10){
+                if(five > 0){
+                    ten++;
+                    five--;
+                }else{
+                    return false;
+                }
+            }else{
+                if(ten > 0 && five > 0){
+                    ten--;
+                    five--;
+                    twenty++;
+                }else if(five > 2){
+                    five -= 3;
+                    twenty++;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
