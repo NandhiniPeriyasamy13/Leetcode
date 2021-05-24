0482439e06c7b444df72c28148b52e88eb49e0eb
Nandhini Periyasamy <nandhiniperiyasamy13@gmail.com>
2021-05-23 20:30:13 -0500

    Time: 0 ms (100.00%), Space: 39.2 MB (80.16%) - LeetHub

---
+++ missing-number/missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = (n*(n+1))/2;
+        
+        int actualSum = 0;
+        for(int i=0;i<n;i++){
+            actualSum += nums[i];
+        }
+        
+        return sum - actualSum;
+    }
+}
