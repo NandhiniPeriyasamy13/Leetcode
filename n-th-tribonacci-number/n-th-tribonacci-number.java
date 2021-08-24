400bd23e42ff5b560ef7e7674bf0fa2c98ee9b56
Nandhini Periyasamy <nandhiniperiyasamy13@gmail.com>
2021-08-24 18:21:02 -0500

    Time: 0 ms (100.00%), Space: 35.4 MB (96.38%) - LeetHub

---
+++ n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n == 0)
+            return 0;
+        
+        if(n == 1 || n == 2)
+            return 1;
+        
+        int[] tri = new int[n+1];
+        
+        tri[0] = 0;
+        tri[1] = 1;
+        tri[2] = 1;
+        
+        for(int i=3;i<=n;i++){
+            tri[i] = tri[i-1]+tri[i-2] + tri[i-3];
+        }
+        
+        return tri[n];
+    }
+}
