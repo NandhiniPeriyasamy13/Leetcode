c1a6378afaaab989f03541da1029fdd5d15c8efd
Nandhini Periyasamy <nandhiniperiyasamy13@gmail.com>
2021-09-13 15:43:24 -0500

    Time: 19 ms (8.92%), Space: 52.4 MB (12.22%) - LeetHub

---
+++ array-partition-i/array-partition-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        
+        int ans = 0;
+        
+        int n = nums.length;
+        for(int i=0;i<n;i+=2){
+            ans += nums[i];
+        }
+        
+        return ans;
+    }
+}
