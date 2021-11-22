058bc741f9d1bda3cc883dff0a7ed8f125c276c6
Nandhini Periyasamy <nandhiniperiyasamy13@gmail.com>
2021-11-22 11:07:10 -0600

    Time: 17 ms (40.00%), Space: 39.1 MB (75.10%) - LeetHub

--- permutation-in-string/permutation-in-string.java
+++ permutation-in-string/permutation-in-string.java
@@ -1,29 +1,42 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        HashMap<Character, Integer> hm = new HashMap<>();
+        HashMap<Character, Integer> patternMap = new HashMap<>();
         
         for(int i=0;i<s1.length();i++){
             char ch = s1.charAt(i);
-            hm.put(ch, hm.getOrDefault(ch,0)+1);
+            patternMap.put(ch, patternMap.getOrDefault(ch,0)+1);
         }
         
         int windowStart = 0;
-        int windowEnd = 0; 
-        while(windowEnd<s2.length()){
-            char ch = s2.charAt(windowEnd);
+        int matchedCount = 0;
+        
+        for(int windowEnd = 0; windowEnd < s2.length(); windowEnd++){
+            char rightChar = s2.charAt(windowEnd);
             
-            if(hm.containsKey(ch)){
-                hm.put(ch, hm.get(ch)-1);
-                if(hm.get(ch) == 0)
-                    hm.remove(ch);
+            if(patternMap.containsKey(rightChar)){
+                patternMap.put(rightChar, patternMap.get(rightChar)-1);
+                if(patternMap.get(rightChar) == 0)
+                    matchedCount++;
+            }
+            
+            
+            if(matchedCount == patternMap.size()){
+                return true;
+            }
+            
+            
+            
+            if(windowEnd >= s1.length()-1){
+                char leftChar = s2.charAt(windowStart);
                 
-                if(hm.size() == 0)
-                    return true;
-            windowEnd++;
-            }else{
-                char ch1 = s2.charAt(windowStart);
-                    hm.put(ch1, hm.getOrDefault(ch1,0)+1);
-                    windowStart++;
+                if(patternMap.containsKey(leftChar)){
+                    if(patternMap.get(leftChar) == 0)
+                        matchedCount--;
+                    
+                    patternMap.put(leftChar, patternMap.get(leftChar)+1);
+                }
+                
+                windowStart++;
             }
             
         }
